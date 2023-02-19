656d62e1e4d127ee41a91b28776d0a077c8b8261
Barros99 <jeanmeneses@hotmail.com>
2023-02-19 11:29:26 -0300

    feat: add sexo attribute

--- lib/model/aluno.dart
+++ lib/model/aluno.dart
@@ -6,12 +6,13 @@ class Aluno {
   final String batizado;
   final String dataBatizado;
   final String dataCadastro;
+  final String sexo;
 
   Aluno(this.nome, this.dataNascimento, this.celular, this.classe,
-      this.batizado, this.dataBatizado, this.dataCadastro);
+      this.batizado, this.dataBatizado, this.dataCadastro, this.sexo);
 
   @override
   String toString() {
-    return '{ nome: $nome, dataNascimento: $dataNascimento, celular: $celular, classe: $classe, batizado: $batizado, dataBatizado: $dataBatizado, dataCadastro: $dataCadastro }\n';
+    return '{ nome: $nome, dataNascimento: $dataNascimento, celular: $celular, classe: $classe, batizado: $batizado, dataBatizado: $dataBatizado, dataCadastro: $dataCadastro, sexo: $sexo }\n';
   }
 }
