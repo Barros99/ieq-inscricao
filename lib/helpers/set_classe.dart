f14c9350622f47843103bf57dca15cd1d2a1cdf0
Barros99 <jeanmeneses@hotmail.com>
2023-02-21 14:56:58 -0300

    feat: set the classe value

---
+++ lib/helpers/set_classe.dart
@@ -0,0 +1,31 @@
+// primeira infância de 0 - 6 anos(72 meses)
+// criança 0 - 11 anos
+// adolescente 12 - 17 anos
+// jovem 18 - 29 anos
+// adulto 30 - 59 anos
+// idoso 60
+
+enum Classe { crianca, adolescente, jovem, homens, mulheres, idoso }
+
+class ClasseHelper {
+  final String sexo;
+  final String dataNascimento;
+
+  ClasseHelper(this.sexo, this.dataNascimento);
+
+  Object get classe {
+    final int idade =
+        DateTime.now().year - int.parse(dataNascimento.substring(6, 10));
+    if (idade < 12) {
+      return Classe.crianca;
+    } else if (idade >= 12 && idade < 18) {
+      return Classe.adolescente;
+    } else if (idade >= 18 && idade < 30) {
+      return Classe.jovem;
+    } else if (idade >= 30 && idade < 60) {
+      return sexo == 'Gender.Male' ? Classe.homens : Classe.mulheres;
+    } else {
+      return Classe.idoso;
+    }
+  }
+}
